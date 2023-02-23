4f2c753f5c5a81d0f64c7cfde6b94222322bab49
pedrolucas0 <siqueirafernandes02@gmail.com>
2023-02-23 16:50:25 -0300

    Nova Estratégia

--- js/scriptExterior.js
+++ js/scriptExterior.js
@@ -3,39 +3,36 @@ const myParam = urlParams.get('word');
 let palavraDigitada = myParam;
 
 document.getElementById("dataDigitada").innerHTML=myParam;
+    
+const encodedParams = new URLSearchParams();
 
-function traduzir(){
+encodedParams.append("q", palavraDigitada);
+encodedParams.append("target", "pt");
+encodedParams.append("source", "de");
 
-    const encodedParams = new URLSearchParams();
+const options = {
+    method: 'POST',
+    headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Accept-Encoding': 'application/gzip',
+        'X-RapidAPI-Key': '5f21641cddmsh4601c9771a0c5e8p1b1289jsn6d601b362759',
+        'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+    },
+    body: encodedParams
+};
 
-    encodedParams.append("q", palavraDigitada);
-    encodedParams.append("target", "pt");
-    encodedParams.append("source", "de");
+fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
+    .then(response => response.json())
+    .then(response => getData(response.data.translations[0].translatedText))
+    .catch(err => console.error(err));
 
-    const options = {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'Accept-Encoding': 'application/gzip',
-            'X-RapidAPI-Key': '5f21641cddmsh4601c9771a0c5e8p1b1289jsn6d601b362759',
-            'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-        },
-        body: encodedParams
-    };
-
-    fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
-        .then(response => response.json())
-        .then(response => getData(palavraDigitada, response))
-        .catch(err => console.error(err));
-
-}
-
-function getData(palavraDigitada, response){
+function getData(data){
 
     //getting the values
-    var palavraTrad = data.response.translation[0].translatedText;
+    let palavraTrad = data;
     console.log(palavraTrad)
-    document.querySelector("#dataTraduzida").innerHTML =
-                `<span class ="card"> ${palavraTrad} </span>
+    let printPalavra = document.querySelector("#palavraTrad")
+    printPalavra.innerHTML =
+                `<span class ="word-card"> ${palavraTrad} </span>
                 `;
 }
