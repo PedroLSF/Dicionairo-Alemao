313d3e988a35053715f55e0b73d75aece0d2dfbb
pedrolucas0 <siqueirafernandes02@gmail.com>
2023-02-23 16:24:54 -0300

    Nova Estratégia

--- js/scriptExterior.js
+++ js/scriptExterior.js
@@ -1,12 +1,42 @@
 const urlParams = new URLSearchParams(window.location.search);
 const myParam = urlParams.get('word');
-console.log(myParam)
+let palavraDigitada = myParam;
 
 document.getElementById("dataDigitada").innerHTML=myParam;
 
-const urlTrads = new URLSearchParams(window.location.search);
-const myTrad = urlTrads.get('wordTrad');
-console.log(myTrad)
+function traduzir(){
 
-document.getElementById("dataTraduzida").innerHTML=myTrad;
+    const encodedParams = new URLSearchParams();
 
+    encodedParams.append("q", palavraDigitada);
+    encodedParams.append("target", "pt");
+    encodedParams.append("source", "de");
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'Accept-Encoding': 'application/gzip',
+            'X-RapidAPI-Key': '5f21641cddmsh4601c9771a0c5e8p1b1289jsn6d601b362759',
+            'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+        },
+        body: encodedParams
+    };
+
+    fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
+        .then(response => response.json())
+        .then(response => getData(palavraDigitada, response))
+        .catch(err => console.error(err));
+
+}
+
+function getData(palavraDigitada, response){
+
+    //getting the values
+    var palavraTrad = data.response.translation[0].translatedText;
+    console.log(palavraTrad)
+    document.querySelector("#dataTraduzida").innerHTML =
+                `<span class ="card"> ${palavraTrad} </span>
+                `;
+
+}
--- js/scriptTraducao.js
+++ js/scriptTraducao.js
@@ -1,37 +1,5 @@
-function traduzir(){
-
-    const encodedParams = new URLSearchParams();
+function enviarPalavra(){
 
     let palavraDigitada = document.querySelector(".word-input").value;
 
-    encodedParams.append("q", palavraDigitada);
-    encodedParams.append("target", "pt");
-    encodedParams.append("source", "de");
-
-    const options = {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'Accept-Encoding': 'application/gzip',
-            'X-RapidAPI-Key': '5f21641cddmsh4601c9771a0c5e8p1b1289jsn6d601b362759',
-            'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-        },
-        body: encodedParams
-    };
-
-    // fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
-    //     .then(response => response.json())
-    //     .then(response => getData(palavraDigitada, response))
-    //     .catch(err => console.error(err));
-    
-    getData("kommen", "Palavra Traduzida")
-
-}
-
-function getData(palavraDigitada, response){
-
-    //getting the values
-    // var palavraTrad = data.response.translation[0];
-    let palavraTrad = response
-    document.querySelector(".word-output").value = palavraTrad;
 }
