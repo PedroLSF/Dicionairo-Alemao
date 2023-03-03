62730dc3123274ab790091d7304a771c8dff5ccd
pedrolucas0 <siqueirafernandes02@gmail.com>
2023-03-03 20:46:32 -0300

    Enviar

--- Modulo2/Entrega2/usuario.controle/usuario.controle.js
+++ Modulo2/Entrega2/usuario.controle/usuario.controle.js
@@ -72,6 +72,8 @@ const readUsuario = (req,res) =>{
     res.render("./usuario.view/Read.html", { usuario : usuarios[index] });
 }
 
+// oi
+
 const updateUsuario = (req,res) =>{
     const nome = req.params.Nome;
     let index = -1;
